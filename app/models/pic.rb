a7163e347ae4357dcf923813688d4cab7f6f1b1c
tsweat <aa@pyby.net>
2011-02-18 12:35:03 -0500

    Moved delete button; removed bottom nav bar

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -4,5 +4,12 @@ class Pic < ActiveRecord::Base
   validates_length_of :img_src, :in => 12..2500
   validates_length_of :setname, :maximum => 25
   validates_length_of :caption, :maximum => 100
+  validate :valid_height
+  
+  def valid_height
+    if img_height.nil? || img_height.blank? || img_height < 1
+      errors.add(:img_height, "couldn't get image height, image URL may be invalid")
+    end
+  end
                                           
 end
