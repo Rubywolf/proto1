e4beb35fefd4f17a14ab50ff0d7ee4677b0b5595
Tracy Sweat <cc@pyby.net>
2011-07-26 21:38:37 -0400

    Forgot the to_i.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -43,7 +43,7 @@ class PicsController < ApplicationController
       pics = []
       allpics.each {|pic| pics.push pic unless pic.setname == "chicks"}
       @pics_count = pics.count
-      @pic_num = (params[:id] >= @pics_count) ? 1 : params[:id]
+      @pic_num = (params[:id].to_i >= @pics_count) ? 1 : params[:id].to_i
  	    @route_string="/show"
     else
       @pic_num = params[:id].to_i
